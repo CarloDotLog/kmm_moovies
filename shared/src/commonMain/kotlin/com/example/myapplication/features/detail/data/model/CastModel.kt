036c65736bee0782e2a133c0487f6351cc2c13f6
carlo.loguercio <carlo.loguercio@accenture.com>
2022-01-21 11:47:20 +0100

    get cast i nit

---
+++ shared/src/commonMain/kotlin/com/example/myapplication/features/detail/data/model/CastModel.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.features.detail.data.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class CastModel(val profile_path: String,
+                val name: String)
